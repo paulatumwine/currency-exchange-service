0312f0378391b8e33efb411740477c135ec74584
Paul A. Tumwine <paulatumwine@gmail.com>
2020-04-08 14:43:00 -0500

    Add sleuth for (distributed) request tracing

--- src/main/java/com/samples/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/samples/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.samples.microservices.currencyexchangeservice.controller;
 
 import com.samples.microservices.currencyexchangeservice.model.ExchangeValue;
 import com.samples.microservices.currencyexchangeservice.repository.ExchangeValueRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     Environment environment;
 
@@ -20,6 +24,7 @@ public class CurrencyExchangeController {
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
         ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from, to);
         exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+        logger.info("{}", exchangeValue);
         return exchangeValue;
     }
 }
